20a6fe435d259b3fe2f61be20c6d86e238b6e88c
Andrey S. Vakhtanov <wahha@mail.ru>
2019-11-07 16:10:53 +0300

    repair skiping projecction, small and capital letters

--- parce_file.py
+++ parce_file.py
@@ -50,14 +50,30 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
     else:
         Center_Marka='Формат??'
     #-----------------------
-    ### СК-42 BL-42 CK-63/42 СК-95 BL-95 ПЗ-90 ПЗ-90.02 WGS-84 BL-WGS
-
-
-
+    ### projection=['СК-42','BL-42','CK-63/42','СК-95','BL-95','ПЗ-90','ПЗ-90.02','WGS-84','BL-WGS']
     #=====================ПРАСИНГ КООРДИНАТ===================================================
+    line_SK42=r' A-00-000-А-а 0000000.00 0000000.00 Hn= 000.00 g00= 0.00 СК-42 ' #3
+    line_BL42=r' 00°00\'00.0000\" 00°00\'00.0000\" g00= 0.00 BL-42 ' #4
+    line_SK63_42=r' A-00- 00-А-А 0000000.00 P0000000.00(0000000.00 P0000000.00) CK-63/42 ' #5
+    line_SK95=r' 0000000.00 0000000.00 Hg= 000.00 СК-95 ' #6
+    line_BL95=r' 00°00\'00.0000\" 00°00\'00.0000\" BL-95 ' #7
+    line_PZ90=r' 0000000.00 0000000.00 0000000.00 ПЗ-90 ' #8
+    line_PZ9002=r' 0000000.00 0000000.00 0000000.00 ПЗ-90.02 ' #9
+    line_WGS84=r' 0000000.00 0000000.00 0000000.00 WGS-84 ' #10
+    line_BLWGS84=r' 00°00\'00.0000\" 00°00\'00.0000\" Hw= 000.00 Gwg= 00.00 BL-WGS ' #11
     
+    for line in GGSBlock:
+        if 'СК-42' in line: line_SK42=line
+        if 'BL-42' in line: line_BL42=line
+        if 'CK-63/42' in line: line_SK63_42=line
+        if 'СК-95' in line: line_SK95=line
+        if 'BL-95' in line: line_BL95=line
+        if 'ПЗ-90' in line: line_PZ90=line
+        if 'ПЗ-90.02' in line: line_PZ9002=line
+        if 'WGS-84' in line: line_WGS84=line
+        if 'BL-WGS' in line: line_BLWGS84=line
     #======================
-    SEARCH = re.search('\s*(.-..-...-\w-\w)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(Hn=\s*-{0,1}\d*\.\d*\s*\S{0,1})?\s*([gG]98=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', GGSBlock[3])
+    SEARCH = re.search('\s*(.-..-...-\w-\w)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(Hn=\s*-{0,1}\d*\.\d*\s*\S{0,1})?\s*([gG]98=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', line_SK42)
     if SEARCH != None :
         Nomenklatura1=SEARCH.groups()[0]
         SK42_X=SEARCH.groups()[1]
@@ -79,7 +95,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         G98 = 'Формат??'
         Proj1 = 'Формат??'
     # -----------------------
-    SEARCH = re.search(r'\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*([gG]87=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*',GGSBlock[4])
+    SEARCH = re.search(r'\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*([gG]87=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*',line_BL42)
     if SEARCH != None:
         SK42_B = SEARCH.groups()[0]
         SK42_L = SEARCH.groups()[1]
@@ -99,7 +115,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         # print('G87:', G87, '.')
         # print('Proj2:', Proj2, '.')
     #-----------------------
-    SEARCH = re.search('\s*(.-..-...-\w-\w)\s*(\d*\.\d*)\s*(\S{0,1}.*\d*\.\d*).*\((\d*\.\d*)\s*(\S{0,1}.*\d*\.\d*)\)\s*(\w{2}-\S*/\S*)\s*', GGSBlock[5])
+    SEARCH = re.search('\s*(.-..-...-\w-\w)\s*(\d*\.\d*)\s*(\S{0,1}.*\d*\.\d*).*\((\d*\.\d*)\s*(\S{0,1}.*\d*\.\d*)\)\s*(\w{2}-\S*/\S*)\s*', line_SK63_42)
     if SEARCH != None :
         Nomenklatura2=SEARCH.groups()[0]
         SK63_X=SEARCH.groups()[1]
@@ -122,7 +138,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         SK63_Y2 = 'Формат??'
         Proj3 = 'Формат??'
     #-----------------------
-    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(Hg=\s*-{0,1}\d*\.\d*)?\s*(EGM=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', GGSBlock[6])
+    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(Hg=\s*-{0,1}\d*\.\d*)?\s*(EGM=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', line_SK95)
     if SEARCH != None :
         SK95_X=SEARCH.groups()[0]
         SK95_Y=SEARCH.groups()[1]
@@ -141,7 +157,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         EGM = 'Формат??'
         Proj4 = 'Формат??'
     # -----------------------
-    SEARCH = re.search('\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\w{2}-\S*)\s*',GGSBlock[7])
+    SEARCH = re.search('\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\w{2}-\S*)\s*',line_BL95)
     if SEARCH != None:
         SK95_B = SEARCH.groups()[0]
         SK95_L = SEARCH.groups()[1]
@@ -154,7 +170,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         SK95_L = 'Формат??'
         Proj5 = 'Формат??'
     #-----------------------
-    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{2}-\S*)\s*', GGSBlock[8])
+    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{2}-\S*)\s*', line_PZ90)
     if SEARCH != None :
         PZ90_X=SEARCH.groups()[0]
         PZ90_Y=SEARCH.groups()[1]
@@ -170,7 +186,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         PZ90_Z = 'Формат??'
         Proj6 = 'Формат??'
     #-----------------------
-    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{2}-\S*)\s*', GGSBlock[9])
+    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{2}-\S*)\s*', line_PZ9002)
     if SEARCH != None :
         PZ9002_X=SEARCH.groups()[0]
         PZ9002_Y=SEARCH.groups()[1]
@@ -186,7 +202,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         PZ9002_Z = 'Формат??'
         Proj7 = 'Формат??'
     #-----------------------
-    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{3}-\S*)\s*', GGSBlock[10])
+    SEARCH = re.search('\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\d*\.\d*)\s*(\w{3}-\S*)\s*', line_WGS84)
     if SEARCH != None :
         WGS84_X=SEARCH.groups()[0]
         WGS84_Y=SEARCH.groups()[1]
@@ -202,7 +218,7 @@ def parce_gg_block_keys(GGSBlock,out_folder,NumberOfGGSs): #РАЗБОР Клю�
         WGS84_Z = 'Формат??'
         Proj8 = 'Формат??'
     #-----------------------
-    SEARCH = re.search('\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(Hw=\s*-{0,1}\d*\.\d*)?\s*(Gwg=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', GGSBlock[11])
+    SEARCH = re.search('\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(\d{2}°\d{2}\'\d{2}\.\d*\")\s*(Hw=\s*-{0,1}\d*\.\d*)?\s*(Gwg=\s*-{0,1}\d*\.\d*)?\s*(\w{2}-\S*)\s*', line_BLWGS84)
     if SEARCH != None :
         WGS84_B=SEARCH.groups()[0]
         WGS84_L=SEARCH.groups()[1]
@@ -252,7 +268,7 @@ def parse_ggs_file(in_file,out_folder):
     'Distant','Comment1','Name_Type','Center_Marka',
     'Proj1','Nomenklatura1','SK42_X','SK42_Y','Hn','G98',
     'Proj2','SK42_B','SK42_L','G87',
-    'Proj3','SK63_X','SK63_Y','SK63_X2','SK63_Y2',
+    'Proj3','Nomenklatura2','SK63_X','SK63_Y','SK63_X2','SK63_Y2',
     'Proj4','SK95_X','SK95_Y','Hg','EGM',
     'Proj5','SK95_L','SK95_B',
     'Proj6','PZ90_X','PZ90_Y','PZ90_Z',
